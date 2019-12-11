78b584aaaf60d9a0e4689ad048471b382170a3cb
Mohbat <mohbattharani1@gmail.com>
2019-12-11 12:30:43 +0500

    Add files via upload

--- tf4.sh
+++ tf4.sh
@@ -1,19 +1,18 @@
-wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.8.0/tensorflow-1.8.0-cp35-none-linux_armv7l.whl
-
-sudo pip3 install tensorflow-1.8.0-cp35-none-linux_armv7l.whl
+sudo apt install libatlas-base-dev -y
+sudo pip3 install tensorflow -y
 sudo pip3 uninstall mock
 sudo pip3 install mock
 
 
-sudo apt-get install python3-numpy
-sudo apt-get install libblas-dev
-sudo apt-get install liblapack-dev
-sudo apt-get install python3-dev 
-sudo apt-get install libatlas-base-dev
-sudo apt-get install gfortran
-sudo apt-get install python3-setuptools
-sudo apt-get install python3-scipy
-sudo apt-get update
-sudo apt-get install python3-h5py
-sudo pip3 install keras 
+sudo apt-get install python3-numpy -y
+sudo apt-get install libblas-dev -y
+sudo apt-get install liblapack-dev -y
+sudo apt-get install python3-dev -y 
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install gfortran -y
+sudo apt-get install python3-setuptools -y
+sudo apt-get install python3-scipy -y
+sudo apt-get update -y
+sudo apt-get install python3-h5py -y
+sudo pip3 install keras -y
 
