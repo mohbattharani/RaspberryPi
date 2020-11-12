41b008815262839f5c5015b7e725f43e4daf9708
mobhattharani <mohbattharani1@gmail.com>
2020-11-12 19:16:45 +0500

    db added

---
+++ db/mongo.py
@@ -0,0 +1,21 @@
+import pymongo
+
+client = pymongo.MongoClient ('mongodb://localhost:27017/',
+                              username='pi', password ='wit1234',
+                              authSource = 'sensors')
+print (client)
+
+sensorsdb = client ['sensors']
+#collection = sensorsdb['cvlab']
+print (sensorsdb)
+#print (collection)
+
+
+reading = {'Temperature': 37, 'Humadity': 25}
+sensorsdb.cvlab.pi.insert_one (reading)
+
+x = sensorsdb.cvlab.find_one ()
+print (x)
+
+
+db.createUser ({user:'pi', pwd:'pi1234', roles:[{role:'userAdminAnyDatabase', db:'admin'}]})
